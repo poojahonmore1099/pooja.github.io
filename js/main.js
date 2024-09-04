dfcaa4ddd7fb35b3bcabf6f3549057c6f6861992
pooja <poojahonmore1099@gmail.com>
2024-09-04 18:09:49 +0530

    new skill  changes

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@
     });
 
 
-    // Typed Initiate
+    
     if ($('.typed-text-output').length == 1) {
         var typed_strings = $('.typed-text').text();
         var typed = new Typed('.typed-text-output', {
